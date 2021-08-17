5ca95a25ce19e831761be67bed0001d14baaa595
Scott Donnan <scott.donnan@me.com>
2021-08-17 11:12:39 -0500

    added some visual css updates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const navButton = document.getElementById('navMenu');
 
 navButton.addEventListener('mouseover', function() {
-    navButton.innerHTML = "<h2>'Now I include this data'</h2>";
+    navButton.innerHTML = "<h2>Now I include this data</h2><a href=''>Biography</a><p></p><a href=''>Resume</a>";
 });
 
 navButton.addEventListener('mouseout', function() {
