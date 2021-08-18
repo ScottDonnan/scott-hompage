6e0b9f2931b3e197d40b64f431b7fd82f6b3d787
Scott Donnan <scott.donnan@me.com>
2021-08-18 10:50:59 -0500

    First major revision

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ navButton.addEventListener('mouseover', function() {
     navButton.innerHTML = "<h2>Now I include this data</h2><a href=''>Biography</a><p></p><a href=''>Resume</a>";
 });
 
-navButton.addEventListener('mouseout', function() {
-    navButton.innerHTML = "Navigation";
-});
+//navButton.addEventListener('mouseout', function() {
+//    navButton.innerHTML = "Navigation";
+//});
 
