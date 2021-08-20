df822107a2207bbe79a590e4d4feca8be88cf62d
Scott Donnan <scott.donnan@me.com>
2021-08-20 09:50:34 -0500

    navigation buttons completed, added some content to sections.

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-const navButton = document.getElementById('navMenu');
+//const navButton = document.getElementById('navMenu');
 
-navButton.addEventListener('mouseover', function() {
-    navButton.innerHTML = "<h2>Now I include this data</h2><a href=''>Biography</a><p></p><a href=''>Resume</a>";
-});
+//navButton.addEventListener('mouseover', function() {
+//    navButton.innerHTML = "<h2>Now I include this data</h2><a href=''>Biography</a><p></p><a href=''>Resume</a>";
+//});
 
 // Variables for navigation menu buttons
 const profButton = document.getElementById('profLink');
 const familyButton = document.getElementById('familyLink');
 const musicButton = document.getElementById('musicLink');
 const contactButton = document.getElementById('contactLink');
+const homeButton = document.getElementById('homeLink');
 
 // Variable for page content accessed by menu buttons
 const familySection = document.getElementById('family');
 const profSection = document.getElementById('professional');
 const musicSection = document.getElementById('music');
 const contactSection = document.getElementById('contact');
-
-
-
+const mainpageSection = document.getElementById('home');
 
 
 profButton.addEventListener('click', function() {
@@ -25,6 +24,7 @@ profButton.addEventListener('click', function() {
     musicSection.style.display = 'none';
     contactSection.style.display = 'none';
     profSection.style.display = 'flex';
+    mainpageSection.style.display = 'none';
 });
 
 familyButton.addEventListener('click', function() {
@@ -32,6 +32,7 @@ familyButton.addEventListener('click', function() {
     musicSection.style.display = 'none';
     contactSection.style.display = 'none';
     profSection.style.display = 'none';
+    mainpageSection.style.display = 'none';
 });
 
 musicButton.addEventListener('click', function() {
@@ -39,6 +40,7 @@ musicButton.addEventListener('click', function() {
     musicSection.style.display = 'flex';
     contactSection.style.display = 'none';
     profSection.style.display = 'none';
+    mainpageSection.style.display = 'none';
 });
 
 contactButton.addEventListener('click', function() {
@@ -46,4 +48,13 @@ contactButton.addEventListener('click', function() {
     musicSection.style.display = 'none';
     contactSection.style.display = 'flex';
     profSection.style.display = 'none';
+    mainpageSection.style.display = 'none';
+});
+
+homeButton.addEventListener('click', function() {
+    familySection.style.display = 'none';
+    musicSection.style.display = 'none';
+    contactSection.style.display = 'none';
+    profSection.style.display = 'none';
+    mainpageSection.style.display = 'flex';
 });
